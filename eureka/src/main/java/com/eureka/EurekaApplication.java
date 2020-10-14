6bed95f66761fe3cbb98eb3821497dd681295621
wangkangxi <1548344039@qq.com>
2020-10-14 14:20:02 +0800

    Initial commit

---
+++ eureka/src/main/java/com/eureka/EurekaApplication.java
@@ -0,0 +1,23 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: kaccn
+ * @description: 服务注册中心
+ * @create: 2020-10-13 13:53
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    /**
+     * 注册中心入口
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
